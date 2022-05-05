b51af36971ef161851ba12595dc49ad01b265167
unknown <manuel.andrei.lopez@gmail.com>
2022-05-04 23:46:17 -0500

    deliverable2

--- MechacarChallenge.R
+++ MechacarChallenge.R
@@ -5,3 +5,10 @@ head(mechacar_mpgdf)
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechacar_mpgdf) #generate multiple linear regression model
 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechacar_mpgdf)) #generate summary statistics
+
+
+suspension_coil <- read.csv('Suspension_Coil.csv',stringsAsFactors = F) #read in dataset
+head(suspension_coil)
+library(tidyverse)
+summarize_coil <- suspension_coil %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') #create summary table with multiple columns
+summarize_coil_lot <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') #create summary table with multiple columns
